sailfishos-mirror/orc

commit c9c39f3482fd4f22b454dfb07aad253ed6c6705f

riscv: Add target
Co-authored-by: Filip Wasil <f.wasil@samsung.com>
Part-of: <https://gitlab.freedesktop.org/gstreamer/orc/-/merge_requests/236>

orc/riscv/orcriscvtarget.c
@@ -1,0 +1,166 @@
+/*
+  Copyright © 2024-2025 Samsung Electronics
+
+  Author: Maksymilian Knust, m.knust@samsung.com
+  Author: Filip Wasil, f.wasil@samsung.com
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions
+  are met:
+  1. Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+  2. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+  IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <orc/orcdebug.h>
+#include <orc/orctarget.h>
+#include <orc/orcinternal.h>
+#include <orc/orcutils-private.h>
+#include <orc/orcutils.h>
+#include <orc/orccode.h>
+#include <orc/riscv/orcriscv-internal.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <ctype.h>
+
+#if defined(HAVE_RISCV) && defined(__linux__)
+#include <sys/cachectl.h>
+
+static int
+orc_riscv_target_detect_extension (const char *exts, const char *ext)
+{
+  while (isspace (*exts))
+    exts++;
+
+  ORC_ASSERT (strncmp (exts, "rv", 2) == 0);
+
+  const int n = strlen (ext);
+  char *sep = strchr (exts, '_');
+
+  if (n == 1) {
+    char *found = strstr (exts + 2, ext);
+    return found && (found < sep || sep == NULL);
+  }
+
+  do {
+    if (strncmp (sep + 1, ext, n) && !isalnum (exts[n]))
+      return TRUE;
+  } while ((sep = strchr (sep + 1, '_')));
+
+  return FALSE;
+}
+
+static orc_uint32
+orc_riscv_target_get_cpu_flags (void)
+{
+  orc_uint32 ret = 0;
+
+#if defined(__riscv_xlen) && __riscv_xlen == 64
+  ret |= ORC_TARGET_RISCV_64BIT;
+#endif
+
+  char *cpuinfo = get_proc_cpuinfo ();
+  if (cpuinfo == NULL) {
+    ORC_DEBUG ("Failed to read /proc/cpuinfo");
+    return 0;
+  }
+
+  char *cpuinfo_line = get_tag_value (cpuinfo, "isa");
+  if (cpuinfo_line) {
+    if (orc_riscv_target_detect_extension (cpuinfo_line, "c"))
+      ret |= ORC_TARGET_RISCV_C;
+
+    if (orc_riscv_target_detect_extension (cpuinfo_line, "v"))
+      ret |= ORC_TARGET_RISCV_V;
+
+    if (orc_riscv_target_detect_extension (cpuinfo_line, "zvkb"))
+      ret |= ORC_TARGET_RISCV_ZVKB;
+
+    if (orc_riscv_target_detect_extension (cpuinfo_line, "zvbb"))
+      ret |= ORC_TARGET_RISCV_ZVBB;
+
+    if (orc_riscv_target_detect_extension (cpuinfo_line, "zvkn"))
+      ret |= ORC_TARGET_RISCV_ZVKN;
+
+    if (orc_riscv_target_detect_extension (cpuinfo_line, "zvks"))
+      ret |= ORC_TARGET_RISCV_ZVKS;
+
+    free (cpuinfo_line);
+  }
+
+  return ret;
+}
+
+#endif
+
+static void
+orc_riscv_target_flush_cache (OrcCode *code)
+{
+#if defined(HAVE_RISCV) && defined(__linux__)
+  __riscv_flush_icache (code->code, code->code + code->code_size, 0);
+#else
+  ORC_ERROR ("Couldn't flush icache");
+  ORC_ASSERT (0);
+#endif
+}
+
+static orc_uint32
+orc_riscv_target_get_default_flags (void)
+{
+#if defined(HAVE_RISCV) && defined(__linux__)
+  return orc_riscv_target_get_cpu_flags ();
+#else
+  return ORC_TARGET_RISCV_64BIT | ORC_TARGET_RISCV_V;
+#endif
+}
+
+static OrcTarget orc_riscv_target = {
+  "riscv",
+#ifdef HAVE_RISCV
+  TRUE,
+#else
+  FALSE,
+#endif
+  ORC_VEC_REG_BASE,
+  orc_riscv_target_get_default_flags,
+  orc_riscv_compiler_init,
+  orc_riscv_compiler_assemble,
+  {{0}},
+  0,
+  NULL,
+  NULL,
+  NULL,
+  orc_riscv_target_flush_cache,
+};
+
+OrcTarget *
+orc_riscv_target_init (void)
+{
+#ifdef HAVE_RISCV
+  if (orc_riscv_target_get_cpu_flags () & ORC_TARGET_RISCV_V) {
+    ORC_INFO ("This RISC-V CPU supports RVV");
+  }
+#endif
+  orc_target_register (&orc_riscv_target);
+  return &orc_riscv_target;
+}

meson.build
@@ -37,7 +37,7 @@ if cc.has_link_argument('-Wl,-Bsymbolic-functions')
 endif
 
 all_targets = ['avx', 'sse', 'mmx']
-extra_targets = ['altivec', 'neon', 'mips', 'c64x'] # 'arm'
+extra_targets = ['altivec', 'neon', 'mips', 'c64x', 'riscv'] # 'arm'
 enabled_targets = []
 
 host_system = host_machine.system()
@@ -79,6 +79,8 @@ elif cpu_family == 'aarch64' and host_system != 'windows'
   cdata.set('HAVE_AARCH64', true)
 elif cpu_family == 'mips' and host_machine.endian() == 'little'
   cdata.set('HAVE_MIPSEL', true)
+elif cpu_family.startswith('riscv')
+  cdata.set('HAVE_RISCV', true)
 else
   warning(cpu_family + ' with ' + host_system + ' isn\'t a supported configuration for optimization')
 endif
@@ -258,6 +260,7 @@ summary({
   'MIPS': 'mips' in enabled_targets,
   'c64x': 'c64x' in enabled_targets,
   'Altivec': 'altivec' in enabled_targets,
+  'RISCV': 'riscv' in enabled_targets,
   }, section: 'Targets', bool_yn: true)
 
 if not have_docs

orc/meson.build
@@ -121,6 +121,8 @@ endif
 orc_lib_args = orc_c_args + ['-DBUILDING_ORC']
 orc_static_cargs = ['-DORC_STATIC_COMPILATION']
 
+subdir('riscv')
+
 orc_lib = library ('orc-' + orc_api,
   orc_sources,
   version : libversion,

orc/orc.c
@@ -74,6 +74,9 @@ orc_init (void)
 #ifdef ENABLE_TARGET_MIPS
       orc_mips_init();
 #endif
+#ifdef ENABLE_TARGET_RISCV
+      orc_riscv_init();
+#endif
 
       inited = TRUE;
     }

orc/orcinternal.h
@@ -178,6 +178,7 @@ ORC_INTERNAL void orc_neon_init (void);
 ORC_INTERNAL void orc_c64x_init (void);
 ORC_INTERNAL void orc_c64x_c_init (void);
 ORC_INTERNAL void orc_mips_init (void);
+ORC_INTERNAL void orc_riscv_init (void);
 
 typedef struct _OrcCodeRegion OrcCodeRegion;
 typedef struct _OrcCodeChunk OrcCodeChunk;

orc/orctarget.h
@@ -26,6 +26,16 @@ typedef enum {
   ORC_TARGET_POWERPC_V207 = (1<<4)
 } OrcTargetPowerPCFlags;
 
+typedef enum {
+  ORC_TARGET_RISCV_64BIT = (1<<0),
+  ORC_TARGET_RISCV_C = (1<<1),
+  ORC_TARGET_RISCV_V = (1<<2),
+  ORC_TARGET_RISCV_ZVKB = (1<<3),
+  ORC_TARGET_RISCV_ZVBB = (1<<4) | ORC_TARGET_RISCV_ZVKB,
+  ORC_TARGET_RISCV_ZVKN = (1<<5) | ORC_TARGET_RISCV_ZVKB,
+  ORC_TARGET_RISCV_ZVKS = (1<<6) | ORC_TARGET_RISCV_ZVKB,
+} OrcTargetRiscvFlags;
+
 enum {
   ORC_TARGET_NEON_CLEAN_COMPILE = (1<<0),
   ORC_TARGET_NEON_NEON = (1<<1),

orc/orcutils-private.h
@@ -30,7 +30,7 @@
 
 ORC_BEGIN_DECLS
 
-#if defined(__arm__) || defined(__aarch64__) || defined(__mips__)
+#if defined(__arm__) || defined(__aarch64__) || defined(__mips__) || defined(__riscv)
 char * get_proc_cpuinfo (void);
 #endif
 

orc/orcutils.c
@@ -62,7 +62,7 @@ orc_malloc(size_t size)
   return ret;
 }
 
-#if defined(__arm__) || defined(__aarch64__) || defined(__mips__)
+#if defined(__arm__) || defined(__aarch64__) || defined(__mips__) || defined(__riscv)
 char *
 get_proc_cpuinfo (void)
 {

commit 5cb29926dcc88dd476ab9f8ca5f0e8c1cb84e17c

riscv: Fix detection when cpuinfo contains no multi-character extension
orc_riscv_target_detection_extension() wrongly assumes that there's
at least one underscore in the ISA string, which isn't true on RISC-V
platforms where no CPU core declares any multi-character
extensions, leading to crashes in strcmp() on the NULL pointer
returned by strsep().

This isn't reproducible on modern kernels and devicetrees, since
CSR/timer-related instructions (zicsr/zifencei) have been separated out
of RISC-V base extension in later RISC-V specifications, and are
necessary for Linux to boot. But it's still problematic with
vendor-provided older devicetree/kernels, or ones generated by QEMU
earlier than v7.1.0 (without commit 5160bacc0638 ("target/riscv: add
zicsr/zifencei to isa_string")).

Fixes: c9c39f3482fd ("riscv: Add target")
Part-of: <https://gitlab.freedesktop.org/gstreamer/orc/-/merge_requests/279>

orc/riscv/orcriscvtarget.c
@@ -60,10 +60,10 @@ orc_riscv_target_detect_extension (const char *exts, const char *ext)
     return found && (found < sep || sep == NULL);
   }
 
-  do {
+  for (; sep; sep = strchr (sep + 1, '_')) {
     if (strncmp (sep + 1, ext, n) && !isalnum (exts[n]))
       return TRUE;
-  } while ((sep = strchr (sep + 1, '_')));
+  }
 
   return FALSE;
 }